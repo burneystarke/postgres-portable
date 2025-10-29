e9bdc3606589073d5bbe0f3f39e58090474466c6
Starke Gardner <starke.gardner@radancy.com>
2025-10-29 15:24:05 -0400

    pg_isready is known to be unreliable. using select.

--- postgres-portable-healthcheck.sh
+++ postgres-portable-healthcheck.sh
@@ -3,7 +3,7 @@
 su postgres -c "exit $(pgbackrest --stanza=$PGBACKREST_STANZA info | grep 'status: error' | wc -l)"
 [ $? -ne 0 ] && echo "pgbackrest is not ready" && exit 1
 
-su postgres -c 'pg_isready --dbname="${POSTGRES_DB}" --username="${POSTGRES_USER}" || [ $? -ne "0" ] && exit 1'
+su postgres -c "psql -U ${POSTGRES_USER} -d ${POSTGRES_DB} -c 'SELECT 1' || exit 1"
 [ $? -ne 0 ] && echo "pg is not ready" && exit 1
 
 CHKSUM_ERROR_COUNT=$(su postgres -c "psql --dbname=${POSTGRES_DB} --username=""${POSTGRES_USER}"" --tuples-only --no-align --command='SELECT COALESCE(SUM(checksum_failures), 0) FROM pg_stat_database'")
